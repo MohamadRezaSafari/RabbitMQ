07eddaacb0cc28b2b4d2a8b52a766d9c7a9dddb1
Mohamad Reza Safari <mrdev1020@gmail.com>
2021-08-02 14:42:15 +0430

    Initial

---
+++ RabbitMQ.Producer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            QueueConsumer.Consume(channel);
+        }
+    }
+}
